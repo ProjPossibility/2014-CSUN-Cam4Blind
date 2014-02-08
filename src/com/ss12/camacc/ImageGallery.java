22680cfbdfee7337c2f5223ed3695aebdfb01ca3
Stefan Eng <stefaneng13@gmail.com>
2014-02-08 12:25:24 -0800

    Working view image from gallery activity

---
+++ src/com/ss12/camacc/ImageGallery.java
@@ -0,0 +1,76 @@
+package com.ss12.camacc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.example.navigationdrawer.R;
+
+public class ImageGallery extends Activity {
+
+    private static int RESULT_LOAD_IMAGE = 1;
+    Button button;
+    ImageView image;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        image = (ImageView) findViewById(R.id.imageView);
+
+        button = (Button) findViewById(R.id.btnChangeImage);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.action_search);
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
+
+    }
+
+}
