ced06a6cd949366e1eb2c3a696e6366f66178980
Noah J Anderson <noah.j.anderson@gmail.com>
2014-02-09 14:22:15 -0800

    newest version of final

---
+++ src/com/blog/ljtatum/camacc/TTS_Engine.java
@@ -0,0 +1,48 @@
+package com.blog.ljtatum.camacc;
+
+import android.speech.tts.UtteranceProgressListener;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import java.util.HashMap;
+
+public class TTS_Engine extends Activity implements TextToSpeech.OnInitListener {
+
+
+
+    //    HashMap hashMap;
+    TextToSpeech textToSpeech;
+
+    public TTS_Engine ( Activity myActivity )
+    {
+        textToSpeech = new TextToSpeech(myActivity, this);
+    }
+
+    public void say(String message)
+    {
+        textToSpeech.speak(message, TextToSpeech.QUEUE_FLUSH, null );
+    }
+    @Override
+    public void onInit(int i) {
+        textToSpeech.setOnUtteranceProgressListener(new Progress_Listener(){
+
+        });
+    }
+    private class Progress_Listener extends UtteranceProgressListener {
+
+
+        @Override
+        public void onStart(String s) {
+
+        }
+
+        @Override
+        public void onDone(String s) {
+
+        }
+
+        @Override
+        public void onError(String s) {
+
+        }
+    }
+}
